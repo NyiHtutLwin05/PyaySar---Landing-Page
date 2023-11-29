cd8a7e4e1ef30fa160113b94066dc71d8717fdcd
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-11-29 21:21:56 +0630

    update contact section

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
 
   const btnOverrideClasses = {
     extends:
-      'w-auto h-[26px] md:w-[132px] md:h-[46px] px-5 py-[15px] rounded-sm md:rounded-xl text-xs md:text-base',
+      'w-auto lg:w-[132px] h-[26px] sm:h-9 lg:h-[46px] px-5 py-[15px] rounded-sm sm:rounded-xl text-xs sm:text-sm lg:text-base',
   };
 
   const [email, setEmail] = useState('');
@@ -21,14 +21,14 @@ const Index = () => {
     <div>
       <form
         action='submit'
-        className='flex justify-between items-center gap-2 border border-black w-auto md:w-[424px] h-[44px] md:h-[62px] rounded-xl px-2 md:ps-5 md:pe-3 py-2 md:py-2.5'
+        className='flex justify-between items-center gap-2 border border-black w-auto sm:w-[370px] lg:w-[400px] h-[44px] sm:h-[54px] lg:h-[62px] rounded-xl px-2 sm:ps-5 sm:pe-3 py-2 sm:py-2.5'
       >
         <input
           type='text'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder='Enter your email address'
-          className='text-[11px] md:text-base leading-5 font-light'
+          className='text-[11px] sm:text-sm lg:text-base leading-5 font-light'
         />
         <Button
           label='Contact Us'
