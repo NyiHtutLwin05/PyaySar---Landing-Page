c3f01240a6e09019582151e81aa342b79f1cf386
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-11-30 15:14:15 +0630

    optimized contact form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import Button from '../Button';
+import { PropTypes } from 'prop-types';
 
-const Index = () => {
+const OverrideClasses = {
+  override: '',
+  extends: '',
+};
+
+const Index = ({ overrideClasses = OverrideClasses, btnLabel }) => {
   // Define CSS classes for styling
 
   const btnOverrideClasses = {
     extends:
-      'w-auto lg:w-[132px] h-[26px] sm:h-9 lg:h-[46px] px-5 py-[15px] rounded-sm sm:rounded-xl text-xs sm:text-sm lg:text-base',
+      'w-auto h-[26px] sm:h-9 lg:h-[46px] px-5 py-[15px] rounded-sm sm:rounded-xl text-xs sm:text-sm lg:text-base',
   };
 
   const [email, setEmail] = useState('');
@@ -21,7 +27,11 @@ const Index = () => {
     <div>
       <form
         action='submit'
-        className='flex justify-between items-center gap-2 border border-black w-auto sm:w-[370px] lg:w-[400px] h-[44px] sm:h-[54px] lg:h-[62px] rounded-xl px-2 sm:ps-5 sm:pe-3 py-2 sm:py-2.5'
+        className={
+          overrideClasses.override
+            ? overrideClasses.override
+            : `flex justify-between items-center gap-2 border border-black w-auto h-[44px] sm:h-[54px] lg:h-[62px] rounded-xl px-2 sm:ps-5 sm:pe-3 py-2 sm:py-2.5 ${overrideClasses.extends}`
+        }
       >
         <input
           type='text'
@@ -31,13 +41,18 @@ const Index = () => {
           className='text-[11px] sm:text-sm lg:text-base leading-5 font-light'
         />
         <Button
-          label='Contact Us'
+          label={btnLabel}
           overrideClasses={btnOverrideClasses}
           action={handleSubmit}
         />
       </form>
     </div>
   );
+};
+
+Index.propTypes = {
+  overrideClasses: PropTypes.object,
+  btnLabel: PropTypes.any.isRequired,
 };
 
 export default Index;
