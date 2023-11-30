2df4e38877cec424d5b07f7198a998074c95e7bb
James <lwinnyihtut0@gmail.com>
2023-11-30 11:45:11 +0630

    Fine-tuned Toast Section

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -3,9 +3,11 @@ import Toast from "../../components/Toast";
 const Hero = () => {
   return (
     <>
+      {/* Calling the Toast component from the Toast file for a delightful user experience */}
       <div>
         <Toast />
       </div>
+      {/* Toast End */}
     </>
   );
 };
