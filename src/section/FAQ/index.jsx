c81011d56ae3c7187a75a7e8bdad2d2eff0adeb2
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-11-30 14:55:52 +0630

    update FAQ section

--- src/section/FAQ/index.jsx
+++ src/section/FAQ/index.jsx
@@ -1,5 +1,20 @@
+import { FAQs } from '../../data';
+import Accordion from '../../components/Accordion';
+import SectionHeader from '../../components/SectionHeader';
+
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <div className='mx-[30px]'>
+      {/* section */}
+      <SectionHeader heading='FAQ' />
+      <p className='mx-auto text-center text-gray text-base md:text-lg leading-5 sm:leading-12 font-light w-[150px] sm:w-auto'>
+        Frequently Asked Questions
+      </p>
+      <div className='my-8'>
+        <Accordion items={FAQs} />
+      </div>
+    </div>
+  );
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
