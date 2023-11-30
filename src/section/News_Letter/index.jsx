86b30a07eee602114852961443462779954b00d9
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-11-30 15:15:08 +0630

    update newsLetter section

--- src/section/News_Letter/index.jsx
+++ src/section/News_Letter/index.jsx
@@ -1,5 +1,39 @@
+import { useMediaQuery } from 'react-responsive';
+import ContactForm from '../../components/ContactForm';
+import NewsLetterTablet from './index.tablet';
+import { ArrowIconSmall } from '../../constants/Icons';
+
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  const isMobileOrTablet = useMediaQuery({ maxWidth: 768 });
+
+  const formOverrideClasses = {
+    extends:'md:w-[400px]'
+  }
+
+  return (
+    <div className='mx-[30px] py-10'>
+      {isMobileOrTablet ? (
+        <NewsLetterTablet />
+      ) : (
+        <div className='relative'>
+          <div className='w-full h-[202px] md:h-[300px] bg-gray rounded-xl md:rounded-2xl'></div>
+
+          <div className='absolute flex flex-col justify-between items-center bottom-2 right-2 w-full h-[300px] rounded-2xl bg-white border border-black p-5'>
+            <div className='text-center pt-5'>
+              <h1 className='text-5xl leading-13 font-bold text-black mb-5'>
+                Don’t miss out !
+              </h1>
+              <p className='text-lg leading-6 font-medium tracking-widest'>
+                Run your business with confidence
+              </p>
+            </div>
+
+            <ContactForm overrideClasses={formOverrideClasses} btnLabel={<ArrowIconSmall />}/>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Newsletter;
--- src/section/News_Letter/index.tablet.jsx
+++ src/section/News_Letter/index.tablet.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import ContactForm from '../../components/ContactForm';
+import { ArrowIconSmall } from '../../constants/Icons';
 
 const NewsLetterTablet = () => {
-  return <div>NewsLetterTablet</div>;
+  const formOverrideClasses = {
+    extends:'sm:w-[511px]'
+  }
+  return (
+    <>
+      <div className='relative'>
+        <div className='w-full h-[202px] sm:h-[223px] bg-gray rounded-xl md:rounded-2xl'></div>
+
+        <div className='absolute flex flex-col justify-between items-center bottom-2 right-2 w-full h-[202px] sm:h-[223px] rounded-xl bg-white border border-black p-2.5 sm:p-5'>
+          <div className='text-center pt-5'>
+            <h1 className='text-5xl/2 sm:text-3xl leading-8 sm:leading-10 font-bold text-black mb-5'>
+              Don’t miss out !
+            </h1>
+            <p className='text-sm sm:text-base leading-5 font-medium tracking-wide sm:tracking-widest'>
+              Run your business with confidence
+            </p>
+          </div>
+
+          <ContactForm  overrideClasses={formOverrideClasses} btnLabel={<ArrowIconSmall />}/>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NewsLetterTablet;
