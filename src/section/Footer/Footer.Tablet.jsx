de35605f1dda805d5e98970e7f4ac8d0e0718d51
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-11-30 15:15:33 +0630

    update footer section

--- src/section/Footer/Footer.Tablet.jsx
+++ src/section/Footer/Footer.Tablet.jsx
@@ -1,5 +1,70 @@
+import { AppleIcon, GooglePlayIcon } from '../../constants/Icons';
+
 const FooterTablet = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='flex justify-between items-start my-10 mx-[30px]'>
+      {/* navigation section */}
+      <div>
+        <div className='flex flex-col gap-2'>
+          {/* TODO: insert your logo here */}
+          <div className='w-[50px] h-[50px] sm:w-[68px] sm:h-[68px] rounded-full border border-black'></div>
+          <div className='text-[#000000] mb-3 sm:flex sm:items-center gap-5'>
+            {/* Brand Information */}
+            <p className='text-base sm:text-lg font-medium leading-6'>
+              Pyaysar
+            </p>
+            <p className='text-sm sm:text-base leading-5 sm:leading-12 font-light'>
+              something are good
+            </p>
+          </div>
+        </div>
+
+        {/* navigation items */}
+        <div className='flex text-[11px] sm:text-base leading-4 sm:leading-5 font-medium text-black w-[180px] sm:w-[370px] justify-between'>
+          <a href='#'>Home</a>
+          <a href='#features'>Features</a>
+          <a href='#about'>About Us</a>
+          <a href='#contact'>Contact</a>
+        </div>
+      </div>
+
+      {/* download app section */}
+
+      <div>
+        <p className='text-base sm:text-lg leading-6 font-medium text-center mb-2 sm:mb-3'>
+          Coming Soon
+        </p>
+        <div>
+          {/* Google Play Download Link */}
+          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-10 sm:h-[57px] mb-1 sm:mb-2'>
+            <GooglePlayIcon />
+            <p className='text-[8px] sm:text-[11px] text-center'>
+              Download on the <br />{' '}
+              <a
+                href='#'
+                className='text-blue text-[11px] sm:text-base leading-4 font-medium'
+              >
+                Google Play
+              </a>
+            </p>
+          </div>
+          {/* App Store Download Link */}
+          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-10 sm:h-[57px] mb-1 sm:mb-2'>
+            <AppleIcon />
+            <p className='text-[8px] sm:text-[11px] text-center'>
+              Download on the <br />{' '}
+              <a
+                href='#'
+                className='text-blue text-[11px] sm:text-base leading-4 font-medium'
+              >
+                App Store
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterTablet;
--- src/section/Footer/Footer.jsx
+++ src/section/Footer/Footer.jsx
@@ -1,5 +1,81 @@
+import { useMediaQuery } from 'react-responsive';
+import { GooglePlayIcon, AppleIcon } from '../../constants/Icons';
+import FooterTablet from './Footer.Tablet';
+import FooterComponent from '../../components/Footer';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const isMobileOrTablet = useMediaQuery({ maxWidth: 768 });
+
+  return (
+    <div>
+      {isMobileOrTablet ? (
+        <FooterTablet />
+      ) : (
+        <div className='flex justify-between items-center my-20 mx-[30px]'>
+          {/* navigation section */}
+          <div>
+            <div className='flex flex-row gap-5 mb-2'>
+              {/* TODO: insert your logo here */}
+              <div className='w-[68px] h-[68px] rounded-full border border-black'></div>
+              <div className='text-[#000000] mb-3'>
+                {/* Brand Information */}
+                <p className='text-2xl font-medium leading-8'>Pyaysar</p>
+                <p className='text-lg leading-12 font-light'>
+                  something are good
+                </p>
+              </div>
+            </div>
+
+            {/* navigation items */}
+            <div className='flex text-lg leading-6 font-medium text-black w-[421px] justify-between'>
+              <a href='#'>Home</a>
+              <a href='#features'>Features</a>
+              <a href='#about'>About Us</a>
+              <a href='#contact'>Contact</a>
+            </div>
+          </div>
+
+          {/* download app section */}
+
+          <div>
+            <p className='text-2xl leading-8 font-medium text-center mb-8'>
+              Coming Soon
+            </p>
+            <div className='md:flex gap-5'>
+              {/* Google Play Download Link */}
+              <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-[15px] py-2.5 cursor-pointer w-[175px] h-[57px] mb-1'>
+                <GooglePlayIcon />
+                <p className='text-sm leading-4 font-normal text-start'>
+                  Download on the <br />{' '}
+                  <a
+                    href='#'
+                    className='text-blue text-lg leading-6 font-medium'
+                  >
+                    Google Play
+                  </a>
+                </p>
+              </div>
+              {/* App Store Download Link */}
+              <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-[15px] py-2.5 cursor-pointer w-[175px] h-[57px] mb-1'>
+                <AppleIcon />
+                <p className='text-sm leading-4 font-normal text-start'>
+                  Download on the <br />{' '}
+                  <a
+                    href='#'
+                    className='text-blue text-lg leading-6 font-medium'
+                  >
+                    App Store
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <FooterComponent />
+    </div>
+  );
 };
 
 export default Footer;
