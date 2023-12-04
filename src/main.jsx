99d234752798bcb09c0fc54a9571e264fc6430e4
James <lwinnyihtut0@gmail.com>
2023-12-04 15:58:04 +0630

    react i18n setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
-
+import "../i18n.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Suspense fallback={<div>Loading ...</div>}>
+        <App />
+      </Suspense>
     </ChakraProvider>
   </React.StrictMode>
 );
