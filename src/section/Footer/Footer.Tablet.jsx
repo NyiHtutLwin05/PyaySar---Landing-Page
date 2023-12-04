6b04768062f34802c0ff00983101ed30db9a60b5
shinthantlwin02 <shinshinshinstl@gmail.com>
2023-12-04 13:31:40 +0630

    Refactor: Improve footer responsiveness

--- src/section/Footer/Footer.Tablet.jsx
+++ src/section/Footer/Footer.Tablet.jsx
@@ -4,7 +4,7 @@ const FooterTablet = () => {
   return (
     <div className='flex justify-between items-start my-20 mx-[30px]'>
       {/* navigation section */}
-      <div>
+      <div className='max-w-[55%]'>
         <div className='flex flex-col gap-2'>
           {/* TODO: insert your logo here */}
           <div className='w-[50px] h-[50px] sm:w-[68px] sm:h-[68px] rounded-full border border-black'></div>
@@ -20,7 +20,7 @@ const FooterTablet = () => {
         </div>
 
         {/* navigation items */}
-        <div className='flex text-[11px] sm:text-base leading-4 sm:leading-5 font-SGmedium text-black w-[180px] sm:w-[370px] justify-between'>
+        <div className='flex flex-wrap gap-2 sm:gap-4 text-[10px] sm:text-base leading-4 sm:leading-5 font-SGmedium text-black'>
           <a href='#'>Home</a>
           <a href='#features'>Features</a>
           <a href='#about'>About Us</a>
@@ -30,32 +30,32 @@ const FooterTablet = () => {
 
       {/* download app section */}
 
-      <div>
+      <div className='max-w-[45%]'>
         <p className='text-base sm:text-lg leading-6 font-SGmedium text-center mb-2 sm:mb-3'>
           Coming Soon
         </p>
         <div>
           {/* Google Play Download Link */}
-          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-10 sm:h-[57px] mb-1 sm:mb-2'>
+          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-12 sm:h-[57px] mb-1 sm:mb-2'>
             <GooglePlayIcon />
-            <p className='text-[8px] sm:text-[11px] text-center'>
+            <p className='text-[8px] sm:text-sm text-center'>
               Download on the <br />{' '}
               <a
                 href='#'
-                className='text-blue text-[11px] sm:text-base leading-4 font-SGmedium'
+                className='text-blue text-[8px] sm:text-base leading-4 font-SGmedium'
               >
                 Google Play
               </a>
             </p>
           </div>
           {/* App Store Download Link */}
-          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-10 sm:h-[57px] mb-1 sm:mb-2'>
+          <div className='flex justify-center items-center gap-2 border border-gray rounded-xl px-2 py-1 cursor-pointer w-auto h-12 sm:h-[57px] mb-1 sm:mb-2'>
             <AppleIcon />
-            <p className='text-[8px] sm:text-[11px] text-center'>
+            <p className='text-[8px] sm:text-sm text-center'>
               Download on the <br />{' '}
               <a
                 href='#'
-                className='text-blue text-[11px] sm:text-base leading-4 font-SGmedium'
+                className='text-blue text-[8px] sm:text-base leading-4 font-SGmedium'
               >
                 App Store
               </a>
